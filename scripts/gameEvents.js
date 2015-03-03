304ee4a0777d55406e46f1a638719cddb1cc2e6a
ChexWarrior <aplehm@gmail.com>
2015-03-03 17:18:58 -0500

    Removed attachDOMevents and replaced with IFFE

--- scripts/gameEvents.js
+++ scripts/gameEvents.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
-    attachDOMEvents();    
+    (function() {
+        $("#numPlayersSelect").on("change", function() {
+            if($(this).val() != "") {
+
+               var boardElement = Snap("#board");
+               $(".hex").remove();
+               var gameBoard = new Board(boardElement, parseInt($(this).val(), 10));
+               $("#playerTurnIndicator").html("Player Turn: 1");
+               $("#endTurnBtn").on("click", function() {
+                    gameBoard.changeTurn();
+               });
+               gameBoard.createBoard();
+               gameBoard.determineStartingHexes();
+               gameBoard.processPlayerTurn();
+            }
+        });
+    })();
 });
-
-function attachDOMEvents() {
-    //num players DDL
-    $("#numPlayersSelect").on("change", function() {
-        if($(this).val() != "") {
-
-           var boardElement = Snap("#board");
-           $(".hex").remove();
-           var gameBoard = new Board(boardElement, parseInt($(this).val(), 10));
-           $("#playerTurnIndicator").html("Player Turn: 1");
-           $("#endTurnBtn").on("click", function() {
-                gameBoard.changeTurn();
-           });
-           gameBoard.createBoard();
-           gameBoard.determineStartingHexes();
-           gameBoard.processPlayerTurn();
-        }
-    });
-}
